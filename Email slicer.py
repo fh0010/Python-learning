7ea0f9edaefeefb62182627af9b75670122ba888
fh0010 <fh0010@gmail.com>
2023-05-06 19:37:36 -0400

    test

--- Email slicer.py
+++ Email slicer.py
@@ -1,8 +1,17 @@
+#請用戶輸入 Email 地址，然後判斷它是自定義域名還是熱門域名。
 print("Email slicer \n")
 
 provider = {"gmail":"Google", "hotmail":"Microsoft", "yahoo":"Yahoo", "outlook":"Microsoft"}
 
-email = input("Please enter your email : ").strip()
+email = input("Please enter your email : ")
+email = ('\033[2;46;41m'+email+'\033[0;0m')
 
-user = email[:email.index("@")]
+username = email.split("@")[0]
+userdoma = email.split("@")[-1]
+userdomain = userdoma.split(".")[0]
 
+
+if userdomain in provider.keys():
+    print (f"It seems {email} is registered under {provider[userdomain]} !")
+else:
+    print (f"({email} is registered under custom domain at {userdomain} !")    
